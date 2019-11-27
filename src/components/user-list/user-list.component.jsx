b3a0810a5b5789c43b7621e9c83fd05ea4601023
Daniel <oculus@Daniels-MacBook-Pro.local>
2019-11-27 00:30:10 -0800

    Updated to use higher order components

--- src/components/user-list/user-list.component.jsx
+++ src/components/user-list/user-list.component.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import React from 'react'
 
-import withData from '../../with-data';
+import withData from '../../with-data'
+
+// class UserList extends React.Component {
+	// constructor(props) {
+	// 	super(props)
+
+  //   this.state = {
+  //     users:[]
+  //   }
+  // }
+  
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(response => response.json())
+  //     .then(data =>
+  //       // console.log(data)
+  //       this.setState({ users: data })
+  //     )
+  //   // console.log(this.state)
+  // }
+
+// 	render() {
+// 		return (
+// 			<div className='container user-list'>
+// 				<h1> Users List </h1>
+// 				{this.state.users.map(user => (
+// 					<div className='post' key={user.id}>
+// 						<h1> {user.name} </h1>
+// 						<h2> {user.email} </h2>
+// 					</div>
+// 				))}
+// 			</div>
+// 		)
+// 	}
+// }
 
 const UserList = ({ data }) => (
   <div className='container user-list'>
@@ -14,4 +48,4 @@ const UserList = ({ data }) => (
   </div>
 );
 
-export default withData(UserList);
+export default withData(UserList)
--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -4,55 +4,55 @@ import withData from '../../with-data'
 
 // Convert this functional component to a class component
 
-class UserProfile extends React.Component {
-	constructor(props) {
-		super(props)
+// class UserProfile extends React.Component {
+	// constructor(props) {
+	// 	super(props)
 
-		this.state = {
-			posts: []
-		}
-	}
+	// 	this.state = {
+	// 		posts: []
+	// 	}
+	// }
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/posts')
-			.then(response => response.json())
-			.then(data =>
-				// console.log(data)
-				this.setState({ posts: data })
-			)
-		// console.log(this.state)
-	}
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/posts')
+	// 		.then(response => response.json())
+	// 		.then(data =>
+	// 			// console.log(data)
+	// 			this.setState({ posts: data })
+	// 		)
+	// 	// console.log(this.state)
+	// }
 
-	render() {
-		// console.log(this.state)
-		return (
-			<div className='container'>
-				<h1>{this.props.name}</h1>
-				<h2>{this.props.email}</h2>
-				Posts:
-				{this.state.posts.map(post => (
-					<div className='post' key={post.id}>
-						<h1>{post.title}</h1>
-						<p> {post.body} </p>
-					</div>
-				))}
-			</div>
-		)
-	}
-}
+// 	render() {
+// 		// console.log(this.state)
+// 		return (
+// 			<div className='container'>
+// 				<h1>{this.props.name}</h1>
+// 				<h2>{this.props.email}</h2>
+// 				Posts:
+// 				{this.state.posts.map(post => (
+// 					<div className='post' key={post.id}>
+// 						<h1>{post.title}</h1>
+// 						<p> {post.body} </p>
+// 					</div>
+// 				))}
+// 			</div>
+// 		)
+// 	}
+// }
 
-// const UserProfile = ({ data, name, email }) => (
-//   <div className='container'>
-//     <h1>{name}</h1>
-//     <h2>{email}</h2>
-//     Posts:
-//     {data.map(post => (
-//       <div className='post' key={post.id}>
-//         <h1>{post.title}</h1>
-//         <p> {post.body} </p>
-//       </div>
-//     ))}
-//   </div>
-// );
+const UserProfile = ({ data, name, email }) => (
+  <div className='container'>
+    <h1>{name}</h1>
+    <h2>{email}</h2>
+    Posts:
+    {data.map(post => (
+      <div className='post' key={post.id}>
+        <h1>{post.title}</h1>
+        <p> {post.body} </p>
+      </div>
+    ))}
+  </div>
+);
 
 export default withData(UserProfile)
